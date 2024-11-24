b6149d736bb14df44732d5873b7df337d0e919c0
Fu_Y <328330246@qq.com>
2024-11-24 15:13:57 +0800

    修改sh

--- all_datasets_for_pred96.sh
+++ all_datasets_for_pred96.sh
@@ -54,99 +54,99 @@ fi
 
 
 
-python -u run.py \
-    --task_name pretrain \
-    --root_path ./datasets/ETT-small/ \
-    --data_path ETTh1.csv \
-    --model_id ETTh1 \
-    --model TimeDART \
-    --data ETTh1 \
-    --features M \
-    --input_len 336 \
-    --e_layers 2 \
-    --d_layers 1 \
-    --enc_in 7 \
-    --dec_in 7 \
-    --c_out 7 \
-    --n_heads 16 \
-    --d_model 32 \
-    --d_ff 64 \
-    --patch_len 2 \
-    --stride 2 \
-    --head_dropout 0.1 \
-    --dropout 0.2 \
-    --time_steps 1000 \
-    --scheduler cosine \
-    --lr_decay 0.9 \
-    --learning_rate 0.0001 \
-    --batch_size 16 \
-    --train_epochs 50 \
-> ./run_log/log_test_win/ETTh1/'TimeDART_pretrain'0.01.log 2>&1
-
-for pred_len in 96; do
-    python -u run.py \
-        --task_name finetune \
-        --is_training 1 \
-        --root_path ./datasets/ETT-small/ \
-        --data_path ETTh1.csv \
-        --model_id ETTh1 \
-        --model TimeDART \
-        --data ETTh1 \
-        --features M \
-        --input_len 336 \
-        --label_len 48 \
-        --pred_len $pred_len \
-        --e_layers 2 \
-        --enc_in 7 \
-        --dec_in 7 \
-        --c_out 7 \
-        --n_heads 16 \
-        --d_model 32 \
-        --d_ff 64 \
-        --patch_len 2 \
-        --stride 2 \
-        --dropout 0.2 \
-        --head_dropout 0.1 \
-        --batch_size 16 \
-        --lr_decay 0.5 \
-        --lradj step \
-        --time_steps 1000 \
-        --scheduler cosine \
-        --patience 3 \
-        --learning_rate 0.0001 \
-        --pct_start 0.3 \
-> ./run_log/log_test_win/ETTh1/'TimeDART_finetune'$pred_len_0.01.log 2>&1
-done
-
-# ETTh2
-python -u run.py \
-     --task_name pretrain \
-    --root_path ./datasets/ETT-small/ \
-    --data_path ETTh2.csv \
-    --model_id ETTh2 \
-    --model TimeDART \
-    --data ETTh2 \
-    --features M \
-    --input_len 336 \
-    --e_layers 2 \
-    --d_layers 1 \
-    --enc_in 7 \
-    --dec_in 7 \
-    --c_out 7 \
-    --n_heads 8 \
-    --d_model 8 \
-    --d_ff 32 \
-    --patch_len 2 \
-    --stride 2 \
-    --head_dropout 0.1 \
-    --dropout 0.2 \
-    --time_steps 1000 \
-    --scheduler cosine \
-    --lr_decay 0.95 \
-    --learning_rate 0.0005 \
-    --batch_size 16 \
-    --train_epochs 50 \
-> ./run_log/log_test_win/ETTh2/'TimeDART_pretrain'0.01.log 2>&1
+#python -u run.py \
+#    --task_name pretrain \
+#    --root_path ./datasets/ETT-small/ \
+#    --data_path ETTh1.csv \
+#    --model_id ETTh1 \
+#    --model TimeDART \
+#    --data ETTh1 \
+#    --features M \
+#    --input_len 336 \
+#    --e_layers 2 \
+#    --d_layers 1 \
+#    --enc_in 7 \
+#    --dec_in 7 \
+#    --c_out 7 \
+#    --n_heads 16 \
+#    --d_model 32 \
+#    --d_ff 64 \
+#    --patch_len 2 \
+#    --stride 2 \
+#    --head_dropout 0.1 \
+#    --dropout 0.2 \
+#    --time_steps 1000 \
+#    --scheduler cosine \
+#    --lr_decay 0.9 \
+#    --learning_rate 0.0001 \
+#    --batch_size 16 \
+#    --train_epochs 50 \
+#> ./run_log/log_test_win/ETTh1/'TimeDART_pretrain'0.01.log 2>&1
+#
+#for pred_len in 96; do
+#    python -u run.py \
+#        --task_name finetune \
+#        --is_training 1 \
+#        --root_path ./datasets/ETT-small/ \
+#        --data_path ETTh1.csv \
+#        --model_id ETTh1 \
+#        --model TimeDART \
+#        --data ETTh1 \
+#        --features M \
+#        --input_len 336 \
+#        --label_len 48 \
+#        --pred_len $pred_len \
+#        --e_layers 2 \
+#        --enc_in 7 \
+#        --dec_in 7 \
+#        --c_out 7 \
+#        --n_heads 16 \
+#        --d_model 32 \
+#        --d_ff 64 \
+#        --patch_len 2 \
+#        --stride 2 \
+#        --dropout 0.2 \
+#        --head_dropout 0.1 \
+#        --batch_size 16 \
+#        --lr_decay 0.5 \
+#        --lradj step \
+#        --time_steps 1000 \
+#        --scheduler cosine \
+#        --patience 3 \
+#        --learning_rate 0.0001 \
+#        --pct_start 0.3 \
+#> ./run_log/log_test_win/ETTh1/'TimeDART_finetune'$pred_len_0.01.log 2>&1
+#done
+#
+## ETTh2
+#python -u run.py \
+#     --task_name pretrain \
+#    --root_path ./datasets/ETT-small/ \
+#    --data_path ETTh2.csv \
+#    --model_id ETTh2 \
+#    --model TimeDART \
+#    --data ETTh2 \
+#    --features M \
+#    --input_len 336 \
+#    --e_layers 2 \
+#    --d_layers 1 \
+#    --enc_in 7 \
+#    --dec_in 7 \
+#    --c_out 7 \
+#    --n_heads 8 \
+#    --d_model 8 \
+#    --d_ff 32 \
+#    --patch_len 2 \
+#    --stride 2 \
+#    --head_dropout 0.1 \
+#    --dropout 0.2 \
+#    --time_steps 1000 \
+#    --scheduler cosine \
+#    --lr_decay 0.95 \
+#    --learning_rate 0.0005 \
+#    --batch_size 16 \
+#    --train_epochs 50 \
+#> ./run_log/log_test_win/ETTh2/'TimeDART_pretrain'0.01.log 2>&1
 
 
 
@@ -175,7 +175,6 @@ for pred_len in 96; do
         --dropout 0.4 \
         --head_dropout 0.1 \
         --batch_size 16 \
-        --gpu 7 \
         --lr_decay 0.5 \
         --lradj step \
         --time_steps 1000 \
@@ -247,7 +246,6 @@ done
 #        --dropout 0.2 \
 #        --head_dropout 0.0 \
 #        --batch_size 64 \
-#        --gpu 5 \
 #        --lr_decay 0.5 \
 #        --lradj decay \
 #        --time_steps 1000 \
@@ -322,7 +320,6 @@ done
 #        --dropout 0.4 \
 #        --head_dropout 0.1 \
 #        --batch_size 64 \
-#        --gpu 5 \
 #        --lr_decay 0.5 \
 #        --lradj step \
 #        --time_steps 1000 \
@@ -393,7 +390,6 @@ done
 #        --dropout 0.2 \
 #        --head_dropout 0.1 \
 #        --batch_size 16 \
-#        --gpu 5 \
 #        --lr_decay 0.8 \
 #        --lradj decay \
 #        --time_steps 1000 \
@@ -463,7 +459,6 @@ done
 #        --dropout 0.2 \
 #        --head_dropout 0.1 \
 #        --batch_size 16 \
-#        --gpu 5 \
 #        --lr_decay 0.5 \
 #        --lradj step \
 #        --time_steps 1000 \
@@ -532,7 +527,6 @@ done
 #        --dropout 0.2 \
 #        --head_dropout 0.1 \
 #        --batch_size 8 \
-#        --gpu 1 \
 #        --lr_decay 0.5 \
 #        --lradj step \
 #        --time_steps 1000 \
@@ -600,7 +594,6 @@ done
 #        --dropout 0.2 \
 #        --head_dropout 0.0 \
 #        --batch_size 16 \
-#        --gpu 6 \
 #        --lr_decay 0.5 \
 #        --lradj step \
 #        --time_steps 1000 \
